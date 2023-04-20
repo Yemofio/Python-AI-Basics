c72823d18bd1cec467dc257fb93924961ee14aab
Yemofio <95033608+Yemofio@users.noreply.github.com>
2023-04-20 13:56:52 +0000

    Create encapsulation-0.py

---
+++ encapsulation-0.py
@@ -0,0 +1,10 @@
+class MyClass:
+    def __init__(self):
+        self._private_attr = 10
+        self.public_attr = 20
+        
+    def _private_method(self):
+        pass
+        
+    def public_method(self):
+        pass
